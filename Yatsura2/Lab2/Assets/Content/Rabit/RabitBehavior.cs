39c0aa114d9098a584b48b1ec0de2294be5de109
IYatsura <yatsura.ivanna@gmail.com>
2017-06-26 22:18:44 +0300

    Homework2

---
+++ Yatsura2/Lab2/Assets/Content/Rabit/RabitBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		//[-1, 1]
+		float value = Input.GetAxis ("Horizontal");
+
+		Animator animator = GetComponent<Animator> ();
+		if (Mathf.Abs(value) > 0) {
+			animator.SetBool ("walking", true);
+		} else {
+			animator.SetBool ("walking", false);
+		}
+
+		/*Animator animator1 = GetComponent<Animator> ();
+		if (Mathf.Abs(value) > 0) {
+			animator.SetBool ("run", true);
+		} else {
+			animator.SetBool ("run", false);
+		}*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Yatsura2/Lab2/Assets/Rabitanimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rabitanimation : HeroRabit {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void FixedUpdate () {
+		//[-1, 1]
+		float value = Input.GetAxis ("Horizontal");
+
+		Animator animator = GetComponent<Animator> ();
+		if (Mathf.Abs (value) > 0) {
+			animator.SetBool ("walking", true);
+		} else {
+			animator.SetBool ("walking", false);
+		}
+
+		//Animator animator = GetComponent<Animator> ();
+		if (this.isGrounded) {
+			animator.SetBool ("jump", true);
+		} else {
+			animator.SetBool ("jump", false);
+		}
+
+	}
+}
